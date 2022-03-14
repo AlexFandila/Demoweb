c73235568106687f353b402735733c9a0aae5091
alex <someonesomeplace.com>
2022-03-14 11:09:29 +0100

    Implementación Spring Security

---
+++ src/main/java/com/nttdata/demoweb/repository/entity/Rol.java
@@ -0,0 +1,43 @@
+package com.nttdata.demoweb.repository.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table
+public class Rol implements GrantedAuthority{
+	
+	@Id
+	@Column
+	private Integer id;
+	
+	@Column
+	private String rol;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getRol() {
+		return rol;
+	}
+
+	public void setRol(String nombre) {
+		this.rol = nombre;
+	}
+
+	@Override
+	public String getAuthority() { // 'ADMIN' -> 'ROLE_ADMIN' ## 'GESTOR' -> 'ROLE_GESTOR'
+		return ("ROLE_"+this.rol).toUpperCase();
+	}
+	
+	
+}
---
+++ src/main/java/com/nttdata/demoweb/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,36 @@
+package com.nttdata.demoweb.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.nttdata.demoweb.repository.UsuarioRepoJPA;
+import com.nttdata.demoweb.repository.entity.Usuario;
+import com.nttdata.demoweb.service.UsuarioService;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService, UserDetailsService{
+	
+	@Autowired
+	UsuarioRepoJPA usuarioDAO;
+
+	@Override
+	public List<Usuario> listarUsuarios() {
+		return usuarioDAO.findAll();
+	}
+
+	@Override
+	public Usuario buscarPorUsername(String username) {
+		return usuarioDAO.findById(username).get();
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return buscarPorUsername(username);
+	}
+
+}
